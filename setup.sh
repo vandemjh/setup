d2d912fb24791d86da04a6dc6116310ccccec0f9
Jack Vandemeulebroecke <38433983+vandemjh@users.noreply.github.com>
2020-04-22 23:16:42 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo apt install git
 echo ===== Cloning repo =====
 echo ".cfg" >> .gitignore
 git clone --bare https://github.com/vandemjh/dotfiles.git $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
 config config --local status.showUntrackedFiles no
 rm .gitignore
