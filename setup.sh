dd9fef73ae826be19528111fdb5f196692b6d63e
Jack Vandemeulebroecke <38433983+vandemjh@users.noreply.github.com>
2020-04-23 00:25:12 +0000

    update with yes/no and warning

--- setup.sh
+++ setup.sh
@@ -1,16 +1,24 @@
-diff /etc/skel/.bash_logout ~/.bashrc
-read -p "Warning! This script will delete your ~/.bashrc file, continue? " -n 1 -r
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+DIFF=$(diff /etc/skel/.bashrc ~/.bashrc)
+if [ "$DIFF" != "" ] 
 then
-    exit 1
+    read -p "Warning! This script will delete your ~/.bashrc file, continue? (y/n) " -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        exit 1
+    fi
 fi
+
 echo ===== Installing git =====
 sudo apt-get -y install git
 echo ===== Cloning repo =====
 echo ".cfg" >> .gitignore
 git clone --bare https://github.com/vandemjh/dotfiles.git $HOME/.cfg
+shopt -s expand_aliases     #Allows aliases in current shell scope
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo ===== Removing blank .bashrc =====
+rm .bashrc
+echo ===== Importing dotfiles =====
 config checkout
 config config --local status.showUntrackedFiles no
 rm .gitignore
